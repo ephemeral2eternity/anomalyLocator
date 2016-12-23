dc1051c6b2554a36cb82cce69cf2c7e424b82f2f
Chen Wang <chenw@Chens-MacBook-Pro.local>
2016-12-23 10:22:54 -0500

    Change time window to label suspects

--- anomalyDiagnosis/thresholds.py
+++ anomalyDiagnosis/thresholds.py
@@ -2,9 +2,9 @@
 
 
 # Define all thresholds for label_suspects and diagnosing anomalies
-server_suspect_th = 300     # 5 minutes
-network_suspect_th = 300    # 5 minutes
-device_suspect_th = 3600    # 1 hour
+server_suspect_th = 60     # 5 minutes
+network_suspect_th = 60    # 5 minutes
+device_suspect_th = 600    # 1 hour
 
 # Define the threshold for QoE anomaly diagnosis
 diagnosis_time_window_minutes = 10
