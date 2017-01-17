3fda30bae28c4e581dc9849d16bee623b5c8fbdc
chenw <chenw@cmu.edu>
2017-01-17 18:11:13 -0500

    change url for get_session_updates_json & get_node_updates_json

--- anomalyDiagnosis/urls.py
+++ anomalyDiagnosis/urls.py
@@ -32,6 +32,6 @@ urlpatterns = [
 	url(r'^edit_network', views.editNetwork, name='editNetwork'),
 	url(r'^get_net_graph_json', views.getJsonNetworkGraph, name='getJsonNetworkGraph'),
 	url(r'^get_net_graph', views.getNetworkGraph, name='getNetworkGraph'),
-	url(r'^get_session_updates_json', views.getSessionUpdatesJson, name='get_session_updates_json'),
-	url(r'^get_node_updates_json', views.getNodeUpdatesJson, name='get_node_updates_json'),
+	url(r'^get_sUpdates_json', views.getSessionUpdatesJson, name='get_session_updates_json'),
+	url(r'^get_nUpdates_json', views.getNodeUpdatesJson, name='get_node_updates_json'),
 ]
