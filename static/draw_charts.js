927009066bb6e7e2e602a63fc29ff5a5566dba23
Chen Wang <chenw@cmu.edu>
2017-03-14 23:08:50 -0400

    Fix pie chart color issues

--- static/draw_charts.js
+++ static/draw_charts.js
@@ -2,90 +2,6 @@
  * Created by Chen Wang on 3/14/2017.
  */
 function draw_pie_chart(json_url){
-    Highcharts.theme = {
-        colors: ['#8B0000', '#FFA500', '#006400'],
-        chart: {
-            backgroundColor: null,
-            style: {
-                fontFamily: 'Signika, serif'
-            }
-        },
-        title: {
-            style: {
-                color: 'black',
-                fontSize: '16px',
-                fontWeight: 'bold'
-            }
-        },
-        subtitle: {
-            style: {
-                color: 'black'
-            }
-        },
-        tooltip: {
-            borderWidth: 0
-        },
-        legend: {
-            itemStyle: {
-                fontWeight: 'bold',
-                fontSize: '18px'
-            }
-        },
-        xAxis: {
-            labels: {
-                style: {
-                    color: '#6e6e70'
-                }
-            }
-        },
-        yAxis: {
-            labels: {
-                style: {
-                    color: '#6e6e70'
-                }
-            }
-        },
-        plotOptions: {
-            series: {
-                shadow: true
-            },
-            candlestick: {
-                lineColor: '#404048'
-            },
-            map: {
-                shadow: false
-            }
-        },
-
-        // Highstock specific
-        navigator: {
-            xAxis: {
-                gridLineColor: '#D0D0D8'
-            }
-        },
-        rangeSelector: {
-            buttonTheme: {
-                fill: 'white',
-                stroke: '#C0C0C8',
-                'stroke-width': 1,
-                states: {
-                    select: {
-                        fill: '#D0D0D8'
-                    }
-                }
-            }
-        },
-        scrollbar: {
-            trackBorderColor: '#C0C0C8'
-        },
-
-        // General
-        background2: '#E0E0E8'
-
-    };
-
-    // Apply the theme
-    Highcharts.setOptions(Highcharts.theme);
     $.getJSON(json_url, function (json) {
         Highcharts.chart('pieChart', {
             chart: {
@@ -128,6 +44,9 @@ function draw_pie_chart(json_url){
                     ['Light', json.light]
                 ]
             }]
+        });
+        Highcharts.setOptions({
+            colors: ['#8B0000', '#FFA500', '#006400']
         });
     });
 }
