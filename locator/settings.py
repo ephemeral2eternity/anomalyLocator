dad5ca9318da1924e8dd94de1ac477934cd9a6f9
Chen Wang <chenw@cmu.edu>
2017-04-09 12:56:51 -0400

    remove admin folder

--- locator/settings.py
+++ locator/settings.py
@@ -153,7 +153,6 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/chenw/anomalyLocator'
 
 STATICFILES_DIRS = (
 	os.path.join(BASE_DIR, "static"),
